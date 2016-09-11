09f6c4cc5d8a6116c4ffc76543d146c71574f518
Pavan Reddy <pavanreddi123@ymail.com>
2016-09-11 10:22:11 +0530

    Add files via upload

---
+++ cardoctor/showcarlist.php
@@ -0,0 +1,54 @@
+<?php
+
+require 'db.config.php';
+
+
+$email = $_POST['email'];
+
+$temp = clean($email);
+
+
+//remove '@' and '.' from email address
+function clean($string) {
+   
+
+   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); 
+}
+
+$cars = $temp . "cars";
+
+$sql = "SELECT id,name,brand,madeyear,regno FROM $cars";
+$result = mysqli_query($con, $sql);
+if (!$result)
+  {
+  echo("Error description: " . mysqli_error($con));
+  }
+
+$posts = array();
+$check = false;
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+	
+    while($row = mysqli_fetch_assoc($result)) {
+
+        $posts[] = array('post'=>$row);
+		 $check = true;
+		 	 
+    }
+	
+}
+ encodearray($posts);
+
+/* Encode array to JSON string */
+function encodearray($posts) {
+   header('Content-type: application/json');
+   echo json_encode(array('posts'=>$posts));
+}
+
+if(!$check){
+	echo "no";
+}
+
+mysqli_close($con);
+
+?>
