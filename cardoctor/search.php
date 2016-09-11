d870198b4355049bd31dc9ddb06bcf21fb829e4d
Pavan Reddy <pavanreddi123@ymail.com>
2016-09-11 22:41:01 +0530

    Add files via upload

--- cardoctor/search.php
+++ cardoctor/search.php
@@ -4,11 +4,64 @@ require 'db.config.php';
 
 $search = $_POST['search'];
 
+$status = $_POST['status'];
+
+
+
+$append = "";
+
+if($status == "true"){
+	
+$brand1 = $_POST['brand1'];
+$brand2 = $_POST['brand2'];
+$brand3 = $_POST['brand3'];
+$brand4 = $_POST['brand4'];
+$brand5 = $_POST['brand5'];
+$brand6 = $_POST['brand6'];
+		   
+	
+	if($brand1 == "Maruti"){
+		$append .= " brand LIKE '%Maruti%'OR";	
+	}
+	
+	if($brand2 == "Honda"){
+		$append .= " brand LIKE '%Honda%'OR";	
+	}
+	
+	if($brand3 == "Tata Motors"){
+		$append .= " brand LIKE '%Tata Motors%'OR";	
+	}
+	
+	if($brand4 == "Hyundai"){
+		$append .= " brand LIKE '%Hyundai%'OR";	
+	}
+	if($brand5 == "Toyata"){
+		$append .= " brand LIKE '%Toyata%'OR";	
+	}
+	if($brand6 == "chevrolet"){
+		$append .= " brand LIKE '%chevrolet%'OR";	
+	}
+	
+	$append .=" brand LIKE '%hello%'";
+	$sql = "SELECT brand,shopname,street,subregion,city,state,country,rating FROM serviceglobal WHERE email IN (SELECT email 
+														   FROM search 
+														   WHERE address LIKE '%$search%') AND ($append)";
+													
+	
+}
+else{
+	
+	$sql = "SELECT brand,shopname,street,subregion,city,state,country,rating FROM serviceglobal WHERE email IN (SELECT email 
+														   FROM search 
+														   WHERE address LIKE '%$search%') ";
+	
+}
+
 //"SELECT shopname FROM serviceglobal WHERE email IN (SELECT * FROM search WHERE address LIKE '%$search%'")"
 
-$sql = "SELECT shopname,street,subregion,city,state,country FROM serviceglobal WHERE email IN (SELECT email 
+/* $sql = "SELECT brand,shopname,street,subregion,city,state,country FROM serviceglobal WHERE email IN (SELECT email 
 														   FROM search 
-														   WHERE address LIKE '%$search%')";
+														   WHERE address LIKE '%hyd%') AND (brand LIKE '%Toy%'OR brand LIKE '%Ma%')"; */
 $result = mysqli_query($con, $sql);
 if (!$result)
   {
@@ -40,5 +93,6 @@ if(!$check){
 	echo "no";
 }
 
+mysqli_close($con);
 
 ?>
