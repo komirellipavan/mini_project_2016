7403cbb1a1bdda83ad2be044f852e62637581eb9
Pavan Reddy <pavanreddi123@ymail.com>
2016-09-25 14:24:11 +0530

    Add files via upload

---
+++ service/repairref.php
@@ -0,0 +1,54 @@
+<?php
+
+require 'db.config.php';
+session_start();
+$email = $_SESSION['email'];
+
+$temp = clean($email);
+
+
+//remove '@' and '.' from email address
+function clean($string) {
+   
+
+   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); 
+}
+
+$repair = $temp . "repair";
+$sql = "SELECT * FROM $repair WHERE status='book'";
+
+$result = mysqli_query($con, $sql);
+if (!$result)
+  {
+  echo("Error description: " . mysqli_error($con));
+  }
+
+$posts = array();
+$check = false;
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+	
+    while($row = mysqli_fetch_assoc($result)) {
+
+        $posts[] = array('post'=>$row);
+		 $check = true;
+		 	 
+    }
+	
+}
+ encodearray($posts);
+
+/* Encode array to JSON string */
+function encodearray($posts) {
+   header('Content-type: application/json');
+   echo json_encode(array('posts'=>$posts));
+}
+
+if(!$check){
+	echo "no";
+}
+	
+
+mysqli_close($con);
+
+?>
